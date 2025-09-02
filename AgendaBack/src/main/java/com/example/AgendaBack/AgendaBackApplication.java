34a6ced716cd6c7dee294666f1237a4dd7752287
alexsant22 <alexandre.santana2201@gmail.com>
2025-09-02 16:22:55 -0300

    Projeto criado

---
+++ AgendaBack/src/main/java/com/example/AgendaBack/AgendaBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.AgendaBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaBackApplication.class, args);
+	}
+
+}
---
+++ AgendaBack/src/test/java/com/example/AgendaBack/AgendaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AgendaBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
